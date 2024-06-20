3ad87b8648f0623ae70981ee61498271033f8a51
Sidhesh-nishad <155049903+Sidhesh-nishad@users.noreply.github.com>
2024-06-20 18:25:11 +0530

    first commit

---
+++ components/landingPage/MainSection.jsx
@@ -0,0 +1,15 @@
+import Amenities from "./Amenities";
+import Features from "./Features";
+import ImageSection from "./ImageSection";
+
+function MainSection() {
+  return (
+    <main>
+      <Features />
+      <Amenities />
+      <ImageSection />
+    </main>
+  );
+}
+
+export default MainSection;
---
+++ components/ui/OfferCard.jsx
@@ -0,0 +1,9 @@
+function OfferCard() {
+  return (
+    <div className="border-l-2 bg-[#00000042] w-[305px]">
+      Rental Returns of UPTO 11%**
+    </div>
+  );
+}
+
+export default OfferCard;
