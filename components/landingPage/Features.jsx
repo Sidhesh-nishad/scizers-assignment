5c62ac3170ead207f9d8b650a13da0750d7200a3
Sidhesh-nishad <155049903+Sidhesh-nishad@users.noreply.github.com>
2024-06-20 20:02:32 +0530

    fixed some bug

--- components/landingPage/Features.jsx
+++ components/landingPage/Features.jsx
@@ -1,20 +1,10 @@
 "use client";
 import FeaturesCard from "../ui/FeaturesCard";
 import { featuresData } from "../../utils";
-import Aos from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
 
 function Features() {
-  useEffect(() => {
-    Aos.init();
-  }, []);
   return (
-    <div
-      className="md:px-[222px] px-[30px] pt-[18px] pb-[20px] items-center flex bg-[#F4F9FF] justify-start lg:justify-center gap-[60px] md:gap-[160px] overflow-x-scroll hide-scrollbar  w-full"
-      data-aos="fade-in"
-      data-aos-duration="5000"
-    >
+    <div className="md:px-[222px] px-[30px] pt-[18px] pb-[20px] items-center flex bg-[#F4F9FF] justify-start lg:justify-center gap-[60px] md:gap-[160px] overflow-x-scroll hide-scrollbar  w-full">
       {featuresData.map((data) => (
         <FeaturesCard data={data} key={data.id} />
       ))}
